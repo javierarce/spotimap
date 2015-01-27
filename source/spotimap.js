5f9df639154856e5ee24a68ab48cb44ffc823965
Javier Arce <javierarce@gmail.com>
2015-01-27 13:01:33 +0100

    removes spaces

--- source/spotimap.js
+++ source/spotimap.js
@@ -730,7 +730,7 @@ App = Backbone.View.extend({
       var sParameterName = sURLVariables[i].split('=');
 
       if (sParameterName[0] == sParam) {
-        return sParameterName[1];
+        return decodeURIComponent(sParameterName[1]);
       }
     }
 
